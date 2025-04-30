0ffdb589f778d3e9e6926d65c29e61a338464f4f
Justin Tian <justin.tian.372@gmail.com>
2025-04-30 17:00:28 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Hello, I'm Justin</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
